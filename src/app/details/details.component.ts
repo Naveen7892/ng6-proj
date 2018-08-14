e8aa126ce3aee2237a84fa7e2cfa189daef80ea7
Naveen Kumar <metanaveen@outlook.com>
2018-08-14 14:52:05 +0530

    Fetching more API for posts and details using RxJS, Service and ActivatedRoute

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+// For HTTP
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+
+// For Route: Accesses routing parameter for id
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -7,9 +14,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  user$: Object;
+
+  constructor(private route: ActivatedRoute, private data: DataService) {
+    this.route.params.subscribe( params => this.user$ = params.id );
+  }
 
   ngOnInit() {
+    this.data.getUser(this.user$).subscribe(
+      data => this.user$ = data 
+    );
   }
 
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// For HTTP
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts$: Object;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getPosts().subscribe(
+      data => this.posts$ = data
+    );
   }
 
 }
